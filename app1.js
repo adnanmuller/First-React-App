66d18d20deec838b2eaa6586266c91c91dc86483
adnanmuller <adnan172@orange.fr>
2020-04-19 20:49:15 +0200

    fix increment bug

--- app1.js
+++ app1.js
@@ -23,12 +23,20 @@ class PeopleList extends React.Component {
             data: [],
             loaded: false,
             loading: false,
-            count:10
+            count:0,
+            prevCount:10
+
+
         }
         this.shoot=this.shoot.bind(this);
         this.add=this.add.bind(this);
         this.sub=this.sub.bind(this);
+
+
     }
+
+
+
 
     componentWillMount() {
       this.setState({loading:true})
@@ -38,32 +46,60 @@ class PeopleList extends React.Component {
           .then(data => this.setState({
                 loaded: true,
                 loading: false,
+                count:10,
                 data
             }))
-    }
+    };
 
-    shoot(a){
-      this.setState({loading:true})
-      fetch('https://randomuser.me/api/?results='+a)
-          .then(response => response.json())
-          .then(obj => obj.results)
-          .then(data => this.setState({
-                loaded: true,
-                loading: false,
-                data
-            }))
+
+
+ /*getSnapshotBeforeUpdate(prevProps, prevState) {
+    var test=prevProps.count;
+    console.log(test);
+  }*/
+
+  newrequest(){
+    this.setState({loading:true})
+    fetch('https://randomuser.me/api/?results='+this.state.count)
+        .then(response => response.json())
+        .then(obj => obj.results)
+        .then(data => this.setState({
+              loaded: true,
+              loading: false,
+              data
+          }))
+  }
+
+
+    shoot(flag){
+      switch (flag) {
+        case "add":
+          this.add(10)
+          this.newrequest();
+          break;
+          case "sub":
+            this.sub(10)
+            this.newrequest();
+            break;
+        default:
+
+      }
+
     }
 
     add(a){
-      this.setState({count:this.state.count+a})
+
+      this.setState({count:this.state.count+=a});
+      console.log("new state 20:"+ this.state.count)
+
     }
 
     sub(a){
-      this.setState({count:this.state.count-a})
+      this.setState({count:this.state.count-=a})
     }
 
     render() {
-      const{count}=this.state
+      const{count, prevCount}=this.state
         const { data, loading, loaded } = this.state
         return (loading) ?
             <div>Loading...</div> :
@@ -76,9 +112,10 @@ class PeopleList extends React.Component {
                             <div className="people_all">  <li className="people_each" key={i} className="people">{first} {last} {age} Years old</li><img src={thumbnail} /></div>
                             )
                 })}
-                <button onClick={()=>this.shoot(count)}>Print New List</button><br/>
-                <button onClick={()=>this.add(10)}>+10</button><span>{count}</span><br/>
-                <button onClick={()=>this.sub(10)}>-10</button><span>{count}</span>
+                <button onClick={()=>this.shoot(count)}>Print New List</button><span className="badge badge-info badge1">Actual people: {count}</span><br/>
+                <button onClick={()=>this.shoot("add")}>+10</button><br/>
+                <button onClick={()=>this.shoot("sub")}>-10</button>
+                <PrevState count={count-10} />
             </ol>
     }
   }
@@ -86,3 +123,11 @@ class PeopleList extends React.Component {
     <PeopleList />,
     document.getElementById('root2')
   )
+
+
+  function PrevState(props){
+    return(
+      <span class="badge badge-info badge1">prev people:<span>{props.count}</span> </span>
+    )
+
+  }
