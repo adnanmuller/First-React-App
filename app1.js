8db2a21d43b76cf99543e4f4c01e9553bd9afb64
adnan <adnan172@orange.fr>
2020-05-03 02:32:46 +0200

    add header react component

--- app1.js
+++ app1.js
@@ -361,3 +361,49 @@ function ControllerInfo(props){
     </div>
   )
 }
+
+class Navigation extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+  }
+  render(){
+    return(
+      <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+
+      {/*  <a className="navbar-brand" href="#">Logo</a>  */}
+
+
+        <ul className="navbar-nav">
+
+        {/*
+          <li className="nav-item">
+            <a className="nav-link" href="#">Link 1</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Link 2</a>
+          </li>
+             */}
+
+
+
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+              HOME
+            </a>
+            <div className="dropdown-menu">
+              <a className="dropdown-item" href="#">Link 1</a>
+              <a className="dropdown-item" href="#">Link 2</a>
+              <a className="dropdown-item" href="#">Link 3</a>
+            </div>
+          </li>
+        </ul>
+      </nav>
+    )
+
+  }
+  }
+
+
+  ReactDOM.render(<Navigation  />,document.getElementById("header"));
